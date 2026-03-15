2d6ca6e72aacbbf5cec6d4194e747d7469427050
VIVIVIciiEND <akirmandinara@gmail.com>
2026-03-15 16:00:04 +0300

    Step 6: Custom color scheme added

--- app/src/main/java/com/example/student_material_design/data/Student.kt
+++ app/src/main/java/com/example/student_material_design/data/Student.kt
@@ -1,4 +1,11 @@
 package com.example.student_material_design.data
 
-class Student {
-}
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Student(
+    @DrawableRes val imageResourceId: Int,
+    @StringRes val name: Int,
+    val age: Int,
+    @StringRes val description: Int
+)
